698bdc0e278faffec998ba2ddaa0b6d5da50eb47
Bob Belderbos <bbelderbos@users.noreply.github.com>
2016-02-19 13:01:18 +0100

    adding testing

---
+++ ch2/test_template.py
@@ -0,0 +1,13 @@
+import unittest
+#from quotes import print_quotes
+
+class TestModule(unittest.TestCase):
+
+  def test_f1(self):
+    pass
+
+  def test_f2(self):
+    pass
+
+if __name__ == "__main__":
+  unittest.main()
