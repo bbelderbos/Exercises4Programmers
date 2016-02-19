6f5dd166bd9f1c6d7ef6b289492649162c896f11
Bob Belderbos <bbelderbos@users.noreply.github.com>
2016-02-19 12:48:06 +0100

    completed 4 and 5

---
+++ ch2/4-mad_lib.py
@@ -0,0 +1,6 @@
+noun = raw_input('Enter a noun: ')
+verb = raw_input('Enter a verb: ')
+adjective = raw_input('Enter a adjective: ')
+adverb = raw_input('Enter a adverb: ')
+print "Do you %s your %s %s %s? That's hilarious!" % \
+  (verb, adjective, noun, adverb)
---
+++ ch2/5-simple_math.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# lessons learned:
+# - test various assumptions: num2 = 0 needed ZeroDivisionError exception
+# - dicts don't have order, so had to use list of tuples to maintain order
+# - so overall great that your assumptions are tested by DOING (even this simple exercise)
+#
+def get_num(n):
+  while True:
+    num = raw_input("What is the %s number? " % n)
+    try:
+      num = int(num)
+    except:
+      print "err: not a number"
+      continue
+    if num < 0:
+      print "err: less than 0"
+      continue
+    return num
+
+def divide(num1, num2):
+  """ watch out for divide by 0 """
+  try:
+    return num1/num2
+  except ZeroDivisionError:
+    return 0
+
+def calcs(num1, num2):
+  return [
+    ("+", num1+num2),
+    ("-", num1-num2),
+    ("*", num1*num2),
+    ("/", divide(num1,num2)),
+  ]
+
+num1 = get_num("first")
+num2 = get_num("second")
+out = []
+results = calcs(num1, num2)
+for op, res in results:
+  out.append("%d%s%d = %d\n" % (num1, op, num2, res))
+print "".join(out)
